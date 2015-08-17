f9d9f533c4f7f18ade93a80702a7550fae05fb62
Pablo Cuadrado <pablocuadrado@gmail.com>
2015-08-17 00:58:28 -0300

    crear las paginas

--- app/controllers/api/v1/pages_controller.rb
+++ app/controllers/api/v1/pages_controller.rb
@@ -1,7 +1,14 @@
 class Api::V1::PagesController < ApplicationController
   respond_to :json
+  skip_before_filter :verify_authenticity_token
 
   def show
     render json: Page.first
   end
+
+  def create
+    page = Page.create(url: params[:page][:url])
+    render json: page
+  end
+
 end
